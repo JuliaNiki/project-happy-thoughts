9a5f1f7595ca0a4988eb2b1d3a91ffb1bf1a3db3
Никитина Юля <nik.iuliiasw@gmail.com>
2023-01-10 11:42:32 +0100

    updated url from heroku

--- code/src/utils/urls.js
+++ code/src/utils/urls.js
@@ -1,3 +1,3 @@
-export const API_URL = "https://julia-happy-thoughts-backend.herokuapp.com/thoughts"
-export const LIKES_URL = (id) => `https://julia-happy-thoughts-backend.herokuapp.com/thoughts/${id}/like`
-export const DELETE_URL = (id) => `https://julia-happy-thoughts-backend.herokuapp.com/thoughts/delete/${id}`
+export const API_URL = "https://project-happy-thoughts-api-dlblb7meha-lz.a.run.app/thoughts"
+export const LIKES_URL = (id) => `https://project-happy-thoughts-api-dlblb7meha-lz.a.run.app/thoughts/${id}/like`
+export const DELETE_URL = (id) => `https://project-happy-thoughts-api-dlblb7meha-lz.a.run.app/thoughts/delete/${id}`
